784211b98b8c9c7ce77215931435e36460654526
Gabriel Alves <85041753+gabrielalves07@users.noreply.github.com>
2023-04-13 18:34:55 -0300

    fazendo upload de imagem referente ao aluno no database

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,6 +14,7 @@ module.exports = {
     'no-console': 'off',
     'class-methods-use-this': 'off',
     'import/first': 'off',
+    camelcase: 'off',
     'no-param-reassign': 'off',
   },
 };
--- app.js
+++ app.js
@@ -9,6 +9,7 @@ import home from './src/routes/home';
 import user from './src/routes/user';
 import token from './src/routes/token';
 import aluno from './src/routes/aluno';
+import foto from './src/routes/foto';
 
 class App {
   constructor() {
@@ -27,6 +28,7 @@ class App {
     this.app.use('/users/', user);
     this.app.use('/tokens', token);
     this.app.use('/alunos', aluno);
+    this.app.use('/foto', foto);
   }
 }
 
